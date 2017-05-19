81c675435ff10aefef0db3996a2472511a01f2ea
jeffbahns <jeffbahns@gmail.com>
2017-05-18 19:07:13 -0700

    added training data to repo, classifier up and running

--- p3.py
+++ p3.py
@@ -1,127 +1,75 @@
+import numpy as np
 from numpy import array, fliplr, flipud, linalg, isnan, subtract, dot, random, multiply
 from random import choice, shuffle
 import mnist_data as md
+import struct
+from sklearn.neural_network import MLPClassifier
 
 # features
-def density(m):
-    # 1's / rows*columns
-    ones = 0
-    for row in m:
-        for item in row:
-            if item == 1:
-                ones += 1
-    total_cells = len(m) * len(m[0])
-    #print("ones : {} , total : {}, density : {}".format(ones,total_cells, (1.0*ones)/total_cells))
-    return (1.0*ones) / total_cells
-
-def symmetry(m, _horizontal):
-    px = array(m)
-    flip = fliplr(px) if _horizontal else flipud(px)
-    x = linalg.norm(subtract(flip, px))
-    y = x / density(px)
-    val = 1 - (x / y)
-    return 1 if isnan(val) else val
-    
-def intersections(m, _max, _horizontal):
-    intersections = 0
-    upper_i = len(m)
-    upper_j = len(m[0])
-    for i in range(0, upper_i if _horizontal else upper_j):
-        _last = 0
-        _intersections = 0
-        for j in range(0, upper_j if _horizontal else upper_i):
-            if _last != (m[i][j] if _horizontal else m[j][i]):
-                _intersections += 1
-                _last = m[i][j] if _horizontal else m[j][i]
-        intersections = max(intersections, _intersections) if _max else max(2,min(intersections, _intersections))
-        #intersections = max(intersections, _intersections) if _max else min(intersections, _intersections)
-    return intersections
-
 def avg_value_by_grid(m):
     values =  [[0 for i in range(7)] for j in range(7)]
+    features = []
     for i in range(len(m)):
         for j in range(len(m[0])):
             values[i // 4][j // 4] += m[i][j]
-    return values
 
-def generate_features(m):
-    densityf = density(m) # 1 density
-    h_symmetryf = symmetry(m, True) # 2 horizontal symmetry
-    v_symmetryf = symmetry(m, False) # 3 vertical symmetry
-    h_max_intersections = intersections(m, True, True) # 4 horizontal max  MINS always == 0 ASK ravi
-    h_min_intersections = intersections(m, False, True) # 5 horizontal min
-    v_max_intersections = intersections(m, True, False) # 6 vertical max
-    v_min_intersections = intersections(m, False, False) # 7 vertical min
-    return [densityf, h_symmetryf, v_symmetryf, h_max_intersections, h_min_intersections, v_max_intersections, v_min_intersections, 1]
+    for row in values:
+        for j in row:
+            features.append(j / 16)
+    return features
 
-def pmatrix(m):
-    for i in range(len(m)):
-        if i%4 == 0:
-            print("|----|----|----|----|----|----|----")
-        for j in range(len(m[0])):
-            if j%4 == 0:
-                print("|", end="")
-            if m[i][j] == 1:
-                print(1, end = "")
-            else:
-                print(0, end = "")
-        print()
+def generate_features(training_data):
+    feature_data = []
+    for img in training_data:
+        feature_data.append((img[0], avg_value_by_grid(img[1]))) # (label, [features])
+    return feature_data
 
-####################################################################################################################################################################################################################################
+def getData():
+    #get training set
+    with open('train-labels-idx1-ubyte','rb') as fl:
+        magic,num = struct.unpack(">II",fl.read(8))
+        label = np.fromfile(fl,dtype=np.int8)
+    with open('train-images-idx3-ubyte','rb') as fi:
+        magic, num, rows, cols = struct.unpack(">IIII",fi.read(16))
+        image = np.fromfile(fi,dtype=np.uint8).reshape(len(label),rows,cols)
+        get_image = lambda idx: (label[idx],image[idx])
+    ret = []
+    for i in range(len(label)):
+        ret.append(get_image(i))
+    return ret
 
-def pocket_perceptron(training_data,unknown_data):
-    num_features = len(training_data[0][0])
-    w = random.rand(num_features) # weights <w0, w2, ... , w7>
-    print("w:",w)
-    eta = .2
-    epoch = 100
-    correct = 0
-    unit_step = lambda x: 1 if x < 0 else 5
-    
-    for i in range(0, epoch):
-        td_copy = training_data[:]
-        shuffle(td_copy)
-        for x, expected in td_copy:
-            result = dot(w, x)
-            error = expected - unit_step(result)
-            w += multiply(eta*error, x)
+##################################
 
-    for x, expected in unknown_data:
-        result = dot(x, w)
-        if unit_step(result) == expected:
-            correct += 1
-        #print("{}: {} -> {} == {}".format(x[:num_features], result, unit_step(result), expected))
-    print("Accuracy :: {} %".format((correct / len(unknown_data)) * 100))
-    #print("w:",w)
+def run_classifier(training_features, training_labels, prediction_features, prediction_labels):
+    classifier = MLPClassifier()
+    classifier.fit(training_features,training_labels)
+    predictions = classifier.predict(prediction_features)
+    errors = 0
+    for p in range(len(predictions)):
+        if predictions[p] != prediction_labels[p]:
+            errors += 1
+    return predictions, errors
 
 def main():
-    one100 = md.one100
-    #one900 = md.one900
-    #five100 = md.five100
-    #five900 = md.five900
+    training_data = generate_features(getData())
+    prediction_data = []
+    test_set_size = 6000; # pulling out random 10% of 60000 features = 6000
+    for i in range(0, test_set_size): 
+        random_idx = random.randint(0,len(training_data))
+        prediction_data.append(training_data.pop(random_idx))
 
-    training_data = []
-    unknown_data = []
+    training_features = [t[1] for t in training_data]
+    training_labels = [t[0] for t in training_data]
+    prediction_features = [t[1] for t in prediction_data]
+    prediction_labels = [t[0] for t in prediction_data]
 
-    pmatrix(one100[0])
-    print(avg_value_by_grid(one100[0]))
+    predictions, errors = run_classifier(training_features, training_labels, prediction_features, prediction_labels)
+    correct = len(predictions) - errors
     
-    return 0
-
-
-    # training data
-    for one in one900:
-        training_data.append((generate_features(one), 1)) # tuple of features ([f0, f1, ..., fn], label)
-    for five in five900:
-        training_data.append((generate_features(five), 5))
-
-    # unknown unknown
-    for one in one100:
-        unknown_data.append((generate_features(one), 1))
-    for five in five100:
-        unknown_data.append((generate_features(five), 5))
-
-    pocket_perceptron(training_data, unknown_data)
+    accuracy = int((float(len(predictions)) - float(errors)) / float(len(predictions)) * 100)
+    print("Training Set Size: {}".format(len(training_data)))
+    print("Test Set Size: {}".format(test_set_size))
+    print("Correct: {}/{}, Accuracy: {} %".format(correct, len(predictions),  accuracy))
     return 0
 
 main()
